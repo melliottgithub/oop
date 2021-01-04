f0a66d3d83950233d4bea45930288285d71cb991
Michael <kllmmc23@gmail.com>
2021-01-03 19:13:21 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict';
+/* Object Oriented Programming */
+
+/*  Principles!!!
+
+    1.Abstraction
+    2.Encapsulation
+    3.Inheritance
+    4.Polymorphism
+
+ */
+
+/* -----OOP: Classes----- */
+
+/*  
+-----instantiation-----
+Class ========> Instance 
+instances from a class
+*/
+
+/* OOP in Javascript: Prototypes */
+
+/* 
+                    (Delegation)
+---------------Prototypal Inheritance--------------
+(Contains methods)           (Can acces those methods)
+------Prototypes <=========== Object-----------------
+each object has a prototype.
+Objects are linked to a prototype object!
+
+*/
+
+/* Creating Prototypes
+    1.Constructor Function
+    2.Classes(ES6)
+    3.Object.create()
+*/
